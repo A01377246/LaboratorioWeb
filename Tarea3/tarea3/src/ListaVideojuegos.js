552e425a1f9ce4a1d7a7c35b833a6b1fe82d5e1d
Humberto Carrillo Gómez (hcarrill) <hcarrill@cisco.com>
2023-03-05 11:06:50 -0600

    Added final  changes to stylesheet

--- Tarea3/tarea3/src/ListaVideojuegos.js
+++ Tarea3/tarea3/src/ListaVideojuegos.js
@@ -7,19 +7,19 @@ import h3ODST from "./imagenes/odst.jfif"
 import './stylesheets/ListaVideojuegos.css'
 
 //arreglo que contiene objetos de videojuego para que sea más fácil acceder a ellos
-const arregloDeVideojuegos = [{nombre: "The Last Of us",
+const arregloDeVideojuegos = [{nombre: "The Last Of  Us",
 desarrollador: "Naughty Dog",
-fechaLanzamiento: "June 14, 2013",
+fechaLanzamiento: "Junio 14, 2013",
 imagen: tlou
 },
 {nombre: "The Legend Of Zelda Twilight Princess",
 desarrollador: "Nintendo",
-fechaLanzamiento: "November 19, 2006",
+fechaLanzamiento: "Noviembre 19, 2006",
 imagen: tloz
 },
 {nombre: "Halo 3 ODST",
 desarrollador: "Bungie",
-fechaLanzamiento: "September 22, 2009",
+fechaLanzamiento: "Septiembre 22, 2009",
 imagen: h3ODST
 }
 ];
@@ -27,20 +27,26 @@ imagen: h3ODST
 const ListaVideojuegos = () => {
     return(
     <Fragment>
-        <p className = "LastOfUs">{arregloDeVideojuegos[0]["nombre"]}</p>
-        <p>{arregloDeVideojuegos[0]["desarrollador"]}</p>
-        <p>{arregloDeVideojuegos[0]["fechaLanzamiento"]}</p>
-        <img src = {arregloDeVideojuegos[0]["imagen"]}></img>
+        <p id= "LastOfUs">{arregloDeVideojuegos[0]["nombre"]}</p>
+        <div className = "gameDescription">
+            <p>{`Desarrollador: ${arregloDeVideojuegos[0]["desarrollador"]}`}</p>
+            <p>{`Fecha de Lanzamiento: ${arregloDeVideojuegos[0]["fechaLanzamiento"]}`}</p>
+        </div>
+        <div className="gameImagePlaceholder"><img src = {arregloDeVideojuegos[0]["imagen"]} className = "gameImage"></img></div>
 
-        <p className = "Zelda">{arregloDeVideojuegos[1]["nombre"]}</p>
-        <p>{arregloDeVideojuegos[1]["desarrollador"]}</p>
-        <p>{arregloDeVideojuegos[1]["fechaLanzamiento"]}</p>
-        <img src = {arregloDeVideojuegos[1]["imagen"]}></img>
+        <p id = "Zelda">{arregloDeVideojuegos[1]["nombre"]}</p>
+        <div className = "gameDescription">
+            <p>{`Desarrollador: ${arregloDeVideojuegos[1]["desarrollador"]}`}</p>
+            <p>{`Fecha de Lanzamiento: ${arregloDeVideojuegos[1]["fechaLanzamiento"]}`}</p>
+        </div>
+        <div className="gameImagePlaceholder"><img src = {arregloDeVideojuegos[1]["imagen"]} className = "gameImage"></img></div>
         
-        <p className = "Halo3">{arregloDeVideojuegos[2]["nombre"]}</p>
-        <p>{arregloDeVideojuegos[2]["desarrollador"]}</p>
-        <p>{arregloDeVideojuegos[2]["fechaLanzamiento"]}</p>
-        <img src = {arregloDeVideojuegos[2]["imagen"]}></img>
+        <p id = "Halo">{arregloDeVideojuegos[2]["nombre"]}</p>
+        <div className = "gameDescription">
+            <p>{`Desarrollador: ${arregloDeVideojuegos[2]["desarrollador"]}`}</p>
+            <p>{`Fecha de Lanzamiento: ${arregloDeVideojuegos[2]["fechaLanzamiento"]}`}</p>
+        </div>
+        <div className="gameImagePlaceholder"><img src = {arregloDeVideojuegos[2]["imagen"]}className = "gameImage"></img></div>
     </Fragment>
     );
 }
