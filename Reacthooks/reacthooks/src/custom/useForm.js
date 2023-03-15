4ae138be515c3f15f1e345402d9d70e5f2b8a9fc
DavidMedinaMedina1 <a01653311@tec.mx>
2023-03-14 22:00:21 -0600

    Lab 5 completo

---
+++ Reacthooks/reacthooks/src/custom/useForm.js
@@ -0,0 +1,18 @@
+import { useState, } from "react";
+
+export const useForm = (initialState = {}) => {
+  const [formState, setFormState] = useState(initialState);
+
+  const handleInputChange = (e) => {
+    setFormState({
+      ...formState,
+      [e.target.name]: e.target.value,
+    });
+  };
+  
+  return [
+    formState, handleInputChange
+  ];
+};
+
+
--- Reacthooks/reacthooks/src/effect/Formulario.js
+++ Reacthooks/reacthooks/src/effect/Formulario.js
@@ -1,36 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useForm } from "../custom/useForm";
 import { Mensaje } from "./Mensaje";
+
 
 export const Formulario = () => {
   //Creamos el estado del formulario, cuyo estado inicial va ser un objeto con email y nombre vacíos.
-  const [formState, setFormState] = useState({
-    nombre: "",
-    email: "",
-  });
+  const [formState, handleInputChange] = useForm({ 
+    nombre: '', 
+    email: '' 
+}); 
+
   //Desestructuramos el estado del formulario
   const { nombre, email } = formState;
-  //Esta función se va invocar cada vez que exista un cambio en alguno de los input text del formulario.
-  const handleInputChange = (e) => {
-    //console.log(e.target.name);
-    //console.log(e.target.value);
-    //Actualizamos el estado del formulario.
-    setFormState({
-      ...formState,
-      [e.target.name]: e.target.value,
-    });
-  };
+
+
   //Este useEffect se va disparar cuando se cree el componente por primera vez, por eso enviamos el arreglo de
   // dependencias vacío.
   useEffect(() => {
-    console.log(
-      "Creación del componente por primera vez: useEffect ejecutado..."
-    );
+    console.log("Creación del componente por primera vez: useEffect ejecutado...");
   }, []);
+
   //Este useEffect se va disparar cada vez que se exista un cambio en formState. Por eso enviamos la variable
   // formState en el arreglo de dependencias.
   useEffect(() => {
     console.log("formState cambió...");
   }, [formState]);
+
   //Este useEffect se va disparar cada vez que se exista un cambio en el email. Por eso enviamos la variable
   // email en el arreglo de dependencias.
   useEffect(() => {
@@ -38,7 +33,7 @@ export const Formulario = () => {
   }, [email]);
   return (
     <>
-      <h1>useEffect</h1>
+      <h1>Lab 5</h1>
       <hr />
       {/*
       Creamos el formulario
@@ -71,7 +66,6 @@ export const Formulario = () => {
       true entonces se evalua '<Mensaje />', y si 'nombre' es false ya no se evalúa '<Mensaje />'
        */}
       {nombre === "Jorge" && <Mensaje />}
-      
     </>
   );
 };
